029447c9f4cdcf9fb76e806fb92cd967f5e587fc
ala <sherhoz123@gmail.com>
2020-09-21 09:18:46 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int* someMethod()
+{
+    int* createOne;
+    delete createOne;
+
+    return createOne;
+
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int* newPtr;
+    delete newPtr;
+
+    int* anotherOne;
+    delete anotherOne;
+
+    int* what;
+    delete what;
+    *what = 3;
+
+    int * newIntro;
+    delete newIntro;
+
+    int* assignThis = someMethod();
+    return 0;
+}
